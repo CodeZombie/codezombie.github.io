3f638afa553bf3f7cbfd236e18145073bfa000e6
Cazum <jeremyclark@live.ca>
2020-01-31 23:42:13 -0500

    Posts and Page Titles work
    Added preliminary functionality for blog posts.
    Also, the page title isn't always "Page Title" now :)

--- js/app.js
+++ js/app.js
@@ -1,60 +1,170 @@
+function getDateString(unixTime){
+    var date = new Date(parseInt(unixTime) * 1000);
+    return ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()] + ", " + date.getFullYear();
+}
 
 var badnoise = new Vue({
     el: '#vue_app',
 
-    components: {'list-item': {
-        props: ['id', 'title', 'tags', 'date', 'image', 'link'],
-        data: function(){
-            return {
-                hovered: false
-            }
+    components: {
+        'project-item': {
+            props: ['id', 'title', 'tags', 'date', 'image', 'link'],
+            data: function(){
+                return {
+                    hovered: false
+                }
+            },
+            methods: {
+                tagClick: function(tag) {
+                    this.$emit('tag-click', tag);
+                },
+                titleClick: function(id, link) {
+                    this.$emit('title-click', {id: id, link: link});
+                },
+                onMouseOver: function() {
+                    this.hovered = true;
+                },
+                onMouseLeave: function() {
+                    this.hovered = false;
+                }
+            },
+            template: `
+            <div class="column">
+                <div class="column five">
+                    <img v-on:mouseover="onMouseOver" v-on:mouseleave="onMouseLeave" v-on:click="titleClick(id, link)" v-bind:class="{hovered: hovered}" v-bind:src="image" style="width:100%;">
+                </div>
+                <div class="column seven">
+                    <div v-bind:class="{hovered: hovered}" v-on:click="titleClick(id, link)" v-on:mouseover="onMouseOver" v-on:mouseleave="onMouseLeave" class="title">{{ title }}</div>
+                    <div class="date"> {{date}} </div>
+                    <span class="tags" v-for="(tag, index) in tags">
+                        <span v-on:click="tagClick(tag)" class="tag">{{ tag }}</span><span v-if="index < tags.length-1">,&nbsp</span>
+                    </span>
+                </div>
+            </div>`
         },
-        methods: {
-            tagClick: function(tag) {
-                this.$emit('tag-click', tag);
+
+
+        'post-item': {
+            props: ['id', 'title', 'tags', 'date', 'content-path'],
+            data: function(){
+                return {
+                    hovered: false
+                }
             },
-            titleClick: function(id, link) {
-                this.$emit('title-click', {id: id, link: link});
+            methods: {
+                tagClick: function(tag) {
+                    this.$emit('tag-click', tag);
+                },
+                titleClick: function(id) {
+                    this.$emit('title-click', id);
+                },
+                onMouseOver: function() {
+                    this.hovered = true;
+                },
+                onMouseLeave: function() {
+                    this.hovered = false;
+                },
+                getDateString: getDateString,
             },
-            onMouseOver: function() {
-                this.hovered = true;
-            },
-            onMouseLeave: function() {
-                this.hovered = false;
-            }
-        },
-        template: `
-        <div class="column">
-            <div class="column five">
-                <img v-on:mouseover="onMouseOver" v-on:mouseleave="onMouseLeave" v-on:click="titleClick(id, link)" v-bind:class="{hovered: hovered}" v-bind:src="image" style="width:100%;">
-            </div>
-            <div class="column seven">
-                <div v-bind:class="{hovered: hovered}" v-on:click="titleClick(id, link)" v-on:mouseover="onMouseOver" v-on:mouseleave="onMouseLeave" class="title">{{ title }}</div>
-                <div class="date"> {{date}} </div>
-                <span class="tags" v-for="(tag, index) in tags">
-                    <span v-on:click="tagClick(tag)" class="tag">{{ tag }}</span><span v-if="index < tags.length-1">,&nbsp</span>
-                </span>
-            </div>
-        </div>`
-      }
+            template: `
+            <div class="column">
+                <div class="column">
+                    <div v-bind:class="{hovered: hovered}" v-on:click="titleClick(id)" v-on:mouseover="onMouseOver" v-on:mouseleave="onMouseLeave" class="title">{{ title }}</div>
+                    <div class="date"> {{getDateString(date)}} </div>
+                    <span class="tags" v-for="(tag, index) in tags">
+                        <span v-on:click="tagClick(tag)" class="tag">{{ tag }}</span><span v-if="index < tags.length-1">,&nbsp</span>
+                    </span>
+                </div>
+            </div>`
+        }
     },
     
     router: new VueRouter({ 
+        //  base:           the user-readable path of the higher-level directory the current page exists in
+        //  page:           the current page we're on.
+        //  previousPath:   the route-able path to the higher-level directory.
         routes: [   
-            {path: '/', name: "index", beforeEnter(to, from, next) {
-                window.location.href = "../"; 
-            }},
-            {path: '/posts', name: "posts"},
-            {path: '/posts/tag/:tag', name: "post-search"},
-            {path: '/posts/:id', name: "post"},
-
-            {path: '/projects', name: "projects"},
-            {path: '/projects/tag/:tag', name: "project-search"},
-
-            {path: '/contact', name: 'contact'},
-
-            {path: '*', name: "404"}
-        ],
+            {
+                path: '/',
+                name: "index", 
+                beforeEnter(to, from, next) {
+                    window.location.href = "../"; 
+                }, 
+                meta: {
+                    previousPath: "/",
+                }
+            },
+            {
+                path: '/posts', 
+                name: "posts", 
+                meta: {
+                    title: "badnoise - posts",
+                    base: "badnoise",
+                    page: "/posts",
+                    previousPath: "/",
+                }
+            },
+            {
+                path: '/posts/tag/:tag', 
+                name: "post-search", 
+                meta: {
+                    title: "badnoise - search results",
+                    base: "badnoise/posts",
+                    /*page: "/tag/" + this.$route.params.tag,*/
+                    previousPath: "/posts",
+                }
+            },
+            {
+                path: '/posts/:id', 
+                name: "post", 
+                meta: {
+                    title: "badnoise - post",
+                    base: "badnoise/posts",
+                    page: "/post",
+                    previousPath: "/posts",
+                }
+            },
+            {
+                path: '/projects', 
+                name: "projects", 
+                meta: {
+                    title: "badnoise - projects",
+                    base: "badnoise",
+                    page: "/projects",
+                    previousPath: "/",
+                }
+            },
+            {
+                path: '/projects/tag/:tag', 
+                name: "project-search", 
+                meta: {
+                    title: "badnoise - search results",
+                    base: "badnoise/posts",
+                    /*page: "/tag/" + this.$route.params.tag,*/
+                    previousPath: "/posts",
+                }
+            },
+            {
+                path: '/contact', 
+                name: 'contact', 
+                meta: {
+                    title: "badnoise - contact",
+                    base: "badnoise",
+                    page: "/contact",
+                    previousPath: '/',
+                }
+            },
+            {
+                path: '*', 
+                name: "404", 
+                meta: {
+                    title: "badnoise - 404 not found",
+                    base: "badnoise",
+                    page: "/???",
+                    previousPath: "/",
+                }
+            }
+        ]
     }),
     data: {
         projects: [
@@ -63,54 +173,27 @@ var badnoise = new Vue({
             {id: 2, title: "taskcrawler", tags: ["android", "sqlite", "java", "MVC"], image: "../res/taskcrawler_demo.png", date: "2018", link: "https://github.com/CodeZombie/TaskCrawler"},
             {id: 3, title: "hella", tags: ["css", "layout"], image: "../res/hella_demo.png", date: "2018", link: "http://badnoise.net/hella/"},
         ],
-
+        posts: [
+            {id: 0, title: "Getting Started with Photogrammetry", tags: ["3D", "Graphics", "Art", "Photogrammetry"], date: 1580523551, contentPath: "photogrammetry.htm"},
+            {id: 1, title: "Why Free Software Matters ", tags: ["Ethics", "Licenses", "Software", "Libre"], date: 1580501551, contentPath: "free_software.htm"},
+        ],
+        activePost: {
+            id: -1,
+            title: "",
+            content: ""
+        }
     },
     computed: {
-        //activeRoute returns renderable data about the current path, broken up into:
-        //  base:           the user-readable path of the higher-level directory the current page exists in
-        //  page:           the current page we're on.
-        //  previousPath:   the route-able path to the higher-level directory.
-        activeRoute: function() {
-            var route = {base: "badnoise", page: "", previousPath: "/"};
-            switch(this.$route.name) {
-                case 'post':
-                    route.base += "/posts";
-                    //route.page = "/" + this.posts[this.$route.params.id].title;
-                    route.previousPath = "/posts";
-                    break;
-                case 'posts':
-                    route.page = "/posts";
-                    route.previousPath = "/";
-                    break;
-                case 'post-search':
-                    route.base += "/posts";
-                    route.page = "/tag/" + this.$route.params.tag;
-                    route.previousPath = "/posts";
-                    break;
-                case 'projects':
-                    route.page = "/projects";
-                    route.previousPath = "/";
-                    break;
-                case 'project-search':
-                    route.base += "/projects"
-                    route.page = "/tag/" + this.$route.params.tag;
-                    route.previousPath = "/projects";
-                    break;
-                case 'contact':
-                    route.page += "/contact";
-                    route.previousPath = '/';
-                    break;
-                default:
-                    route.page = "/???";
-                    route.previousPath = "/";
-                    break;
-            }
-            return route;
-        }
     },
     methods: {
         atRoute: function(pageName) {
             return this.$route.name == pageName;
+        },
+        getRoutePage: function() {
+            if(this.atRoute('post')){
+                return "/" + this.activePost.title.toLowerCase();
+            }
+            return this.$route.meta.page;
         },
         navigate: function(path) {
             if(path == '') {
@@ -119,10 +202,10 @@ var badnoise = new Vue({
             this.$router.push({path});
         },
         goBack: function() {
-            this.navigate(this.activeRoute.previousPath);
+            this.navigate(this.$route.meta.previousPath);
         },
-        onPostClick: function(projectObject) {
-            this.navigate('/posts/' + projectObject.id);
+        onPostClick: function(id) {
+            this.navigate('/posts/' + id);
         },
         onPostTagClick: function(tag) {
             this.navigate('/posts/tag/' + tag);
@@ -132,8 +215,48 @@ var badnoise = new Vue({
         },
         onProjectClick: function(projectObject) {
             window.location.href = projectObject.link;
+        },
+        onRouteChange: function(to) {
+            if(to.name == 'post') {
+                if(this.posts[to.params.id] != null){
+                    this.loadPostContent(to.params.id);
+                }else{
+                    this.navigate("/404");
+                }
+            }
+            document.title = "badnoise" + this.getRoutePage();//this.$route.meta.title;
+        },
+        getDateString: getDateString,
+        //loads a post and its content into this.data from an Id.
+        loadPostContent: function(postId) {
+            
+            this.activePost.content = "";
+            if(this.posts[postId] != null){
+                this.activePost.id = postId;
+                this.activePost.title = this.posts[postId].title
+                this.activePost.date = this.posts[postId].date;
+                axios.get("/posts/" + this.posts[postId].contentPath).then( (result) => {
+                    this.activePost.content = result.data
+                }).catch((error) => {
+                    this.activePost.content = "Failed to fetch post content. " + error;
+                })
+                
+            }else{
+                this.activePost.id = -1;
+                this.activePost.title = "Err"
+                this.activePost.content = "Err"
+            }
         }
+    },
+    
+    watch: {
+        //called when the router tries to change routes.
+        '$route' (to, from) {
+            this.onRouteChange(to);
+        }
+    },
+    //called when the browser tries to load a page from a fresh request.
+    mounted: function() {
+        this.onRouteChange(this.$route);
     }
 });
-
-
